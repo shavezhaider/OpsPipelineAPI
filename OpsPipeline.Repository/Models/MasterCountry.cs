e6024690ef380b12a45acd8d37c830d8a2d4be29
shavez <shavezhaider110@gmail.com>
2022-03-18 20:56:55 +0530

    ReportModel

---
+++ OpsPipeline.Repository/Models/MasterCountry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace OpsPipelineAPI.Repository.Models
+{
+    public partial class MasterCountry
+    {
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+        public int IsActive { get; set; }
+        public int IsDeleted { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
